6f5b596be5b4dbcf9196aeb80afd36b9e507cbf7
td2thinh <td2thinh@gmail.com>
2022-11-27 17:03:49 +0700

    Assignment 1 done

---
+++ CaesarCipher/WordPlay.java
@@ -0,0 +1,57 @@
+
+/**
+ * Write a description of WordPlay here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class WordPlay {
+    public boolean isVowel (char ch)
+    {
+        String vowels = "aeiou";
+        if (vowels.indexOf(Character.toLowerCase(ch)) == -1)
+        {
+            return false;
+        }
+        return true;
+    }
+    
+    public String replaceVowels(String phrase, char ch)
+    {
+        StringBuilder phraseSB = new StringBuilder(phrase);
+        
+        for (int i = 0; i < phrase.length(); i++)
+        {
+            if (isVowel(phraseSB.charAt(i)))
+            {
+                phraseSB.setCharAt(i, ch);
+            }
+        }
+        
+        String result = new String(phraseSB);
+        
+        return result;
+    }
+    
+    public String emphasize (String phrase, char ch)
+    {
+        StringBuilder sb = new StringBuilder(phrase);
+        
+        for (int i = 0; i < phrase.length(); i++)
+        {
+            if (sb.charAt(i) == ch)
+            {
+                if (i % 2 == 0)
+                {
+                    sb.setCharAt(i) = '*';
+                }
+                else
+                {
+                    sb.setCharAt(i) = '+';
+                }
+            }
+        }
+        
+        return new String(sb);
+    }
+}
